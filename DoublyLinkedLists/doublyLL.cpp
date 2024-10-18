efe77c83d8e97001d56055458a2e35de2fbefe2e
Michael Garramone <99839019+MikeGarra@users.noreply.github.com>
2024-10-18 16:16:47 -0400

    Add files via upload

--- DoublyLinkedLists/doublyLL.cpp
+++ DoublyLinkedLists/doublyLL.cpp
@@ -53,6 +53,31 @@ class DoublyLinkedList {
         cout << "Length: " << length << endl;
     }
 
+    Node* get(int index) {
+        if (index <0 ||index >= length) return nullptr; 
+        Node* temp = head;
+        if (index < length/2) {
+            for(int i = 0; i < index; i++) {
+                temp = temp->next;
+            }
+        } else {
+            temp = tail;
+            for (int i = length - 1; i > index; i--) {
+                temp = temp->prev;
+            }
+        }
+        return temp;
+    }
+
+    bool set(int index, int value) {
+        Node* temp = get(index);
+        if (temp) {
+            temp->value = value;
+            return true;
+        }
+        return false;
+    }
+
     void append(int value) {
         Node* newNode = new Node(value);
         if (length == 0) {
@@ -79,6 +104,27 @@ class DoublyLinkedList {
         length++;
     }
 
+    bool insert(int index, int value) {
+        if (index < 0 || index > length) return false;
+            if (index == 0) {
+                prepend(value);
+                return true;
+            }
+            if (index == length) {
+                append(value);
+                return true;
+            }
+            Node* newNode = new Node(value);
+            Node* before = get (index - 1);
+            Node* after = before->next;
+            newNode->prev = before;
+            newNode->next = after;
+            before->next = newNode;
+            after->prev = newNode;
+            length++;
+            return true;
+    }
+
     void deleteLast() {
         if (length == 0) return;
         Node* temp = tail;
@@ -94,7 +140,30 @@ class DoublyLinkedList {
     }
 
     void deleteFirst() {
+        if (length ==0) return;
+        Node* temp = head;
+        if (length == 1) {
+            head = nullptr;
+            tail = nullptr;
+        } else {
+            head = head->next;
+            head->prev = nullptr;
+        }
+        delete temp;
+        length--;
+    }
 
+    void deleteNode(int index) {
+        if (index < 0 || index >= length) return;
+        if (index == 0) return deleteFirst();
+        if (index == length - 1) return deleteLast();
+        
+        Node* temp = get(index);
+
+        temp->next->prev = temp->prev;
+        temp->prev->next = temp->next;
+        delete temp;
+        length--;
     }
 };
 
@@ -113,7 +182,31 @@ int main () {
     cout << endl << "Prepended DLL:" << endl;
     myDLL->printList();
 
-    // Removing the last node in the DLL: 
+    // Insert of a new node in the DLL:
+    cout << "Insert of new Value: " << endl;
+    myDLL->insert(1, 99);
+    myDLL->printList();
+
+    // Locating an index with Get Function:
+    cout << "\nValue at index 2 is: ";
+    cout << myDLL->get(2)->value <<endl;
+
+    // Changing a value in the DLL using Set function:
+    myDLL->set(1, 55);
+    cout << "\nSet Function implemented: " << endl;
+    myDLL->printList();
+
+    // Delete a specific Node in DLL:
+    cout << "\nDeletion of specific node:" << endl;
+    myDLL->deleteNode(3);
+    myDLL->printList();
+
+    // Removing the first node in DLL:
+    myDLL->deleteFirst();
+    cout << "\nDLL after first deleteFirst function:\n";
+    myDLL->printList();
+
+    // Removing the last node in DLL: 
     myDLL->deleteLast();
     cout << "\nDLL after first deleteLast function:\n";
     myDLL->printList();
