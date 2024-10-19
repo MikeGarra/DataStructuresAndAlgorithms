50b8b813bca73a2887043dfda750709655b89aa4
Michael Garramone <99839019+MikeGarra@users.noreply.github.com>
2024-10-19 15:31:06 -0400

    Add files via upload

--- StackAndQueues/queues/queues.cpp
+++ StackAndQueues/queues/queues.cpp
@@ -34,6 +34,7 @@ class Queue {
             cout << temp->value << endl;
             temp = temp->next;
         }
+        cout << endl;
     }
 
     void getFirst() {
@@ -48,6 +49,36 @@ class Queue {
         cout <<"Length: " << length << endl;
     }
 
+    void enqueue(int value) {
+        Node* newNode = new Node(value);
+        if (length == 0) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last->next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
+
+    int dequeue() {
+        if (length == 0) {
+            cout << "Queue is empty." << endl;
+            return INT_MIN;
+        }
+        Node* temp = first;
+        int dequeueValue = first->value;
+        cout << "Dequeued value: " << dequeueValue << endl;
+        if (length == 1) {
+            first = nullptr;
+            last = nullptr;
+        } else {
+            first = first->next;
+        }
+        delete temp;
+        length--;
+        return dequeueValue;
+    }
 };
 
 int main() {
@@ -57,5 +88,22 @@ int main() {
     myQueue->getLast();
     myQueue->getLenght();
 
+    myQueue->printQueue();
+
+    // Adding a new Node to the Queue with enqueue function:
+    myQueue->enqueue(43);
+    myQueue->printQueue();
+
+    // Removing a node fromt the Queue with dequeue function:
+    cout << "Removing Node from Queue:" << endl;
+    myQueue->dequeue();
+    myQueue->printQueue();
+
+    cout << "Removing Node from Queue:" << endl;
+    myQueue->dequeue();
+    myQueue->printQueue();
+    
+    cout << "Removing Node from Queue:" << endl;
+    myQueue->dequeue();
     myQueue->printQueue();
 }
