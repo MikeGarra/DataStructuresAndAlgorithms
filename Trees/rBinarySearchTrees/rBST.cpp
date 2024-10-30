e37e2143b525e4e85d920a7a30d4212903a69945
Michael Garramone <99839019+MikeGarra@users.noreply.github.com>
2024-10-30 14:35:26 -0400

    Add files via upload

--- Trees/rBinarySearchTrees/rBST.cpp
+++ Trees/rBinarySearchTrees/rBST.cpp
@@ -28,60 +28,81 @@ class BinarySearchTree {
            root = nullptr; 
         }
 
-    bool insert(int value) {
-        Node* newNode = new Node(value);
-        if (root == nullptr) {
-            root = newNode;
-            return true;
-        }
-        Node* temp = root;
-        while (true) {
-            if (newNode->value == temp->value) return false;
-            if (newNode->value < temp->value) {
-                if (temp->left == nullptr){
-                temp->left = newNode;
+        Node* getRoot() {
+            return root;
+        } 
+
+        bool insert(int value) {
+            Node* newNode = new Node(value);
+            if (root == nullptr) {
+                root = newNode;
                 return true;
             }
-            temp = temp->left;         
-            } else {
-                if (temp->right == nullptr) {
-                    temp->right = newNode;
+            Node* temp = root;
+            while (true) {
+                if (newNode->value == temp->value) return false;
+                if (newNode->value < temp->value) {
+                    if (temp->left == nullptr){
+                    temp->left = newNode;
                     return true;
                 }
-                temp = temp->right;
-            } 
-        }
-    }
-
-    bool contains(int value) {
-        // not needed due to while loop
-        // if (root == nullptr) return false;
-        Node* temp = root;
-        while ( temp) {
-            if (value < temp->value) {
-                temp = temp->left;
-            } else if (value > temp->value) {
-                temp = temp->right;
-            } else {                
-                return true;
+                temp = temp->left;         
+                } else {
+                    if (temp->right == nullptr) {
+                        temp->right = newNode;
+                        return true;
+                    }
+                    temp = temp->right;
+                } 
             }
         }
-        return false;
-    }          
 
-    bool rContains(Node* currentNode, int value) {
-        if (currentNode == nullptr) return false;
-        if (currentNode->value == value) return true;
-        if (value < currentNode->value) {
-            return rContains(currentNode->left, value);
-        } else {
-            return rContains(currentNode->right, value);
+        bool contains(int value) {
+            // not needed due to while loop
+            // if (root == nullptr) return false;
+            Node* temp = root;
+            while ( temp) {
+                if (value < temp->value) {
+                    temp = temp->left;
+                } else if (value > temp->value) {
+                    temp = temp->right;
+                } else {                
+                    return true;
+                }
+            }
+            return false;
+        }          
+
+        //Recursive Code Function Implementation:
+        bool rContains(Node* currentNode, int value) {
+            if (currentNode == nullptr) return false;
+            if (currentNode->value == value) return true;
+            if (value < currentNode->value) {
+                return rContains(currentNode->left, value);
+            } else {
+                return rContains(currentNode->right, value);
+            }
         }
-    }
 
-    bool rContains(int value) {
-        return rContains(root, value);
-    }
+        bool rContains(int value) {
+            return rContains(root, value);
+        }
+
+        Node* rInsert(Node* currentNode, int value) {
+            if (currentNode == nullptr) return new Node(value);
+
+            if (value < currentNode->value) {
+                currentNode->left = rInsert(currentNode->left, value);
+            } else if (value > currentNode->value) {
+                currentNode->right = rInsert(currentNode->right, value);
+            }
+            return currentNode;
+        }
+
+        void rInsert(int value) {
+            if (root == nullptr) root = new Node(value);
+            rInsert(root, value);
+        }
 
 };
 
@@ -105,6 +126,16 @@ int main() {
     cout << myBST->rContains(27); 
 
     cout << "\nBST Contains 17:\n";
-    cout << myBST->rContains(17); 
+    cout << myBST->rContains(17);
 
+    BinarySearchTree* my_rBST = new BinarySearchTree();
+
+    my_rBST->rInsert(2);
+    my_rBST->rInsert(1);
+    my_rBST->rInsert(3);
+
+    cout << "\n\n-----------------------------------";
+    cout << "\n\nRoot: " << my_rBST->getRoot()->value;
+    cout << "\n\nRoot->left: " << my_rBST->getRoot()->left->value;
+    cout << "\n\nRoot->right: " << my_rBST->getRoot()->right->value;
 }
